9fc3282188fbed6fbddd7625f448f3e0e621c0a5
sneha597 <snehat.k1601@gmail.com>
2024-12-05 00:35:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function appendValue(value) {
+    const result = document.getElementById('result');
+    result.value += value;
+}
+
+function clearResult() {
+    const result = document.getElementById('result');
+    result.value = '';
+}
+
+function calculateResult() {
+    const result = document.getElementById('result');
+    try {
+        result.value = eval(result.value); // Evaluate the mathematical expression
+    } catch (error) {
+        result.value = 'Error';
+    }
+}
